86280cb22cda2b3a1375737160f7e323d471e446
Justin Lovinger <git@justinlovinger.com>
2023-09-28 22:53:13 +0000

    Reduce recursion in `organize`

--- src/organize.rs
+++ src/organize.rs
@@ -1,6 +1,5 @@
 use std::{collections::HashSet, iter::once};
 
-use auto_enums::auto_enum;
 use itertools::Itertools;
 
 use crate::{types::MoveOp, Tag, TaggedFile, DIR_SEPARATOR, INLINE_SEPARATOR, TAG_END};
@@ -9,20 +8,20 @@ pub fn organize(files: Vec<TaggedFile>) -> impl Iterator<Item = MoveOp> {
     _organize(files, String::new(), HashSet::new(), 0)
 }
 
-#[auto_enum(Iterator)]
 fn _organize(
-    files: Vec<TaggedFile>,
+    mut files: Vec<TaggedFile>,
     prefix: String,
     used: HashSet<Tag>,
     parent_count: usize,
 ) -> impl Iterator<Item = MoveOp> {
-    if let Some(tag) = tag_to_split(&files, &used) {
-        let (with_tag, without_tag) = files
-            .into_iter()
-            .partition::<Vec<_>, _>(|file| file.tags().contains(&tag.as_ref()));
-        let count = with_tag.len();
-        Box::new(
-            _organize(
+    let mut moves = Vec::new();
+    loop {
+        if let Some(tag) = tag_to_split(&files, &used) {
+            let (with_tag, without_tag) = files
+                .into_iter()
+                .partition::<Vec<_>, _>(|file| file.tags().contains(&tag.as_ref()));
+            let count = with_tag.len();
+            moves.extend(_organize(
                 with_tag,
                 if parent_count == 0 {
                     format!("{}{tag}", &prefix)
@@ -33,21 +32,23 @@ fn _organize(
                 },
                 used.iter().cloned().chain(once(tag)).collect(),
                 count,
-            )
-            .chain(_organize(without_tag, prefix, used, parent_count)),
-        ) as Box<dyn Iterator<Item = _>>
-    } else {
-        let prefix = if parent_count == 0 {
-            prefix
-        } else if parent_count == 1 {
-            format!("{}{INLINE_SEPARATOR}", &prefix)
+            ));
+            files = without_tag;
         } else {
-            format!("{}{DIR_SEPARATOR}", &prefix)
-        };
-        files.into_iter().map(move |file| MoveOp {
-            to: format!("{}{TAG_END}{}", &prefix, file.name()).into(),
-            from: file.into(),
-        })
+            let prefix = if parent_count == 0 {
+                prefix
+            } else if parent_count == 1 {
+                format!("{}{INLINE_SEPARATOR}", &prefix)
+            } else {
+                format!("{}{DIR_SEPARATOR}", &prefix)
+            };
+            return moves
+                .into_iter()
+                .chain(files.into_iter().map(move |file| MoveOp {
+                    to: format!("{}{TAG_END}{}", &prefix, file.name()).into(),
+                    from: file.into(),
+                }));
+        }
     }
 }
 
