0911fc2b8d339a0a373193c5c6fb7ff8e04885c0
Justin Lovinger <git@justinlovinger.com>
2023-10-01 13:19:30 +0000

    Optimize inline tags in `organize`

--- src/organize.rs
+++ src/organize.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
-use auto_enums::auto_enum;
 use itertools::Itertools;
+use smallvec::SmallVec;
 
 use crate::{types::MoveOp, TagRef, TaggedFile, DIR_SEPARATOR, INLINE_SEPARATOR, TAG_END};
 
@@ -18,6 +18,7 @@ pub fn organize(files: &[TaggedFile]) -> impl Iterator<Item = MoveOp> + '_ {
     }
     _organize(
         Files {
+            inline_tags: extract_inline_tags(&mut tags_files),
             tags_files,
             untagged_files: Vec::new(), // Files with no tags will never move.
         },
@@ -26,7 +27,6 @@ pub fn organize(files: &[TaggedFile]) -> impl Iterator<Item = MoveOp> + '_ {
     )
 }
 
-#[auto_enum(Iterator)]
 fn _organize(
     mut files: Files,
     prefix: String,
@@ -36,88 +36,62 @@ fn _organize(
     loop {
         if let Some((tag, count)) = tag_to_split(&files.tags_files) {
             debug_assert_ne!(count, 0);
-            // Singleton tags are common,
-            // making optimizing for them worthwhile.
-            if count == 1 {
-                debug_assert_eq!(
-                    files.tags_files.values().map(|xs| xs.len()).max().unwrap(),
-                    1
-                );
-                return moves
-                    .into_iter()
-                    .chain(to_move_ops(
-                        files.untagged_files,
-                        final_prefix(&prefix, parent_count),
-                    ))
-                    .chain(organize_singleton_tags(
-                        files.tags_files,
-                        prefix,
-                        parent_count,
-                    ));
-            } else {
-                let tag = tag.to_owned();
-                let (with_tag, without_tag) = files.partition(&tag);
-                moves.extend(_organize(
-                    with_tag,
-                    if parent_count == 0 {
-                        format!("{}{tag}", &prefix)
-                    } else if count == parent_count {
-                        format!("{}{INLINE_SEPARATOR}{tag}", &prefix)
-                    } else {
-                        format!("{}{DIR_SEPARATOR}{tag}", &prefix)
-                    },
-                    count,
-                ));
-                files = without_tag;
-            }
-        } else {
-            return moves.into_iter().chain(to_move_ops(
-                files.untagged_files,
-                final_prefix(prefix, parent_count),
+            let tag = tag.to_owned();
+            let (with_tag, without_tag) = files.partition(&tag);
+            moves.extend(_organize(
+                with_tag,
+                if parent_count == 0 {
+                    format!("{}{tag}", &prefix)
+                } else if count == parent_count {
+                    format!("{}{INLINE_SEPARATOR}{tag}", &prefix)
+                } else {
+                    format!("{}{DIR_SEPARATOR}{tag}", &prefix)
+                },
+                count,
             ));
+            files = without_tag;
+        } else {
+            debug_assert_eq!(files.tags_files.len(), 0);
+            return moves
+                .into_iter()
+                .chain(to_move_ops(files, final_prefix(prefix, parent_count)));
         }
     }
 }
 
-fn organize_singleton_tags(
-    tags_files: TagsFiles,
-    prefix: String,
-    parent_count: usize,
-) -> impl Iterator<Item = MoveOp> + '_ {
-    let unused_tags: HashSet<_> = tags_files.keys().cloned().collect();
-    let prefix = final_prefix(prefix, parent_count);
-    tags_files
-        .into_values()
-        .flatten()
-        .unique()
-        .map(move |file| MoveOp {
-            to: format!(
-                "{}{}{TAG_END}{}",
-                &prefix,
-                file.tags()
-                    .filter(|tag| unused_tags.contains(tag))
-                    .sorted_by(|tag, other| tag
-                        .len()
-                        .cmp(&other.len())
-                        .reverse()
-                        .then_with(|| tag.cmp(other)))
-                    .format_with("", |tag, f| {
+fn to_move_ops(files: Files, prefix: String) -> impl Iterator<Item = MoveOp> + '_ {
+    files
+        .untagged_files
+        .into_iter()
+        .map({
+            let prefix = prefix.clone();
+            move |file| MoveOp {
+                to: format!("{}{TAG_END}{}", &prefix, file.name()).into(),
+                from: file.as_path().into(),
+            }
+        })
+        .chain(files.inline_tags.into_iter().map(move |(file, mut tags)| {
+            tags.sort_unstable_by(|tag, other| {
+                tag.len()
+                    .cmp(&other.len())
+                    .reverse()
+                    .then_with(|| tag.cmp(other))
+            });
+            MoveOp {
+                to: format!(
+                    "{}{}{TAG_END}{}",
+                    &prefix,
+                    tags.into_iter().format_with("", |tag, f| {
                         f(&tag)?;
                         f(&INLINE_SEPARATOR)?;
                         Ok(())
                     }),
-                file.name()
-            )
-            .into(),
-            from: file.as_path().into(),
-        })
-}
-
-fn to_move_ops(files: UntaggedFiles, prefix: String) -> impl Iterator<Item = MoveOp> + '_ {
-    files.into_iter().map(move |file| MoveOp {
-        to: format!("{}{TAG_END}{}", &prefix, file.name()).into(),
-        from: file.as_path().into(),
-    })
+                    file.name()
+                )
+                .into(),
+                from: file.as_path().into(),
+            }
+        }))
 }
 
 fn final_prefix(prefix: impl std::fmt::Display, parent_count: usize) -> String {
@@ -147,11 +121,14 @@ fn tag_to_split<'a>(tags_files: &'a TagsFiles) -> Option<(&'a TagRef, usize)> {
 struct Files<'a> {
     tags_files: TagsFiles<'a>,
     untagged_files: UntaggedFiles<'a>,
+    inline_tags: InlineTags<'a>,
 }
 
 type TagsFiles<'a> = HashMap<&'a TagRef, HashSet<WithID<&'a TaggedFile>>>;
 
 type UntaggedFiles<'a> = Vec<WithID<&'a TaggedFile>>;
+
+type InlineTags<'a> = HashMap<WithID<&'a TaggedFile>, SmallVec<[&'a TagRef; 1]>>;
 
 impl<'a> Files<'a> {
     /// Return (with_tag, without_tag).
@@ -159,6 +136,7 @@ impl<'a> Files<'a> {
         let files = self.tags_files.remove(tag).unwrap();
         let mut tags_files: TagsFiles = HashMap::new();
         let mut untagged_files = Vec::new();
+        let mut inline_tags = HashMap::new();
         for file in files {
             let mut added = false;
             for tag in file.tags() {
@@ -172,18 +150,46 @@ impl<'a> Files<'a> {
                     }
                 }
             }
-            if !added {
-                untagged_files.push(file);
+            match self.inline_tags.remove(&file) {
+                Some(tags) => {
+                    inline_tags.insert(file, tags);
+                }
+                None => {
+                    if !added {
+                        untagged_files.push(file)
+                    }
+                }
             }
         }
+        extract_inline_tags_into(&mut tags_files, &mut inline_tags);
+        extract_inline_tags_into(&mut self.tags_files, &mut self.inline_tags);
         (
             Self {
                 tags_files,
                 untagged_files,
+                inline_tags,
             },
             self,
         )
     }
+}
+
+fn extract_inline_tags<'a>(tags_files: &mut TagsFiles<'a>) -> InlineTags<'a> {
+    let mut inline_tags: InlineTags = HashMap::new();
+    extract_inline_tags_into(tags_files, &mut inline_tags);
+    inline_tags
+}
+
+fn extract_inline_tags_into<'a>(tags_files: &mut TagsFiles<'a>, inline_tags: &mut InlineTags<'a>) {
+    tags_files.retain(|tag, tag_files| {
+        if tag_files.len() == 1 {
+            let file = *tag_files.iter().next().unwrap();
+            inline_tags.entry(file).or_default().push(tag);
+            false
+        } else {
+            true
+        }
+    });
 }
 
 /// More efficiently hash and compare
