2cd158d5c1738b92fa652da5bae8fa6b59147e22
Justin Lovinger <git@justinlovinger.com>
2023-10-06 11:47:37 +0000

    Refactor `organize`
    Put `Files` in a module
    for better encapsulation.

--- src/organize.rs
+++ src/organize.rs
@@ -1,37 +1,21 @@
-use std::{
-    collections::{HashMap, HashSet},
-    rc::Rc,
-};
+use std::rc::Rc;
 
-use by_address::ByThinAddress;
 use itertools::Itertools;
-use smallvec::SmallVec;
 
 use crate::{types::MoveOp, Tag, TagRef, TaggedFile, DIR_SEPARATOR, INLINE_SEPARATOR, TAG_END};
+
+use self::files::{Files, TagsFiles};
 
 type Prefix = Vec<(Rc<Tag>, usize)>; // (tag, count)
 
 pub fn organize(files: &[TaggedFile]) -> impl Iterator<Item = MoveOp> + '_ {
-    let mut tags_files: TagsFiles = HashMap::new();
-    for file in files.iter().map(ByThinAddress) {
-        for tag in file.tags() {
-            tags_files.entry(tag).or_default().insert(file);
-        }
-    }
-    _organize(
-        Files {
-            inline_tags: extract_inline_tags(&mut tags_files),
-            tags_files,
-            untagged_files: Vec::new(), // Files with no tags will never move.
-        },
-        Default::default(),
-    )
+    _organize(Files::new(files), Default::default())
 }
 
 fn _organize(mut files: Files, prefix: Prefix) -> impl Iterator<Item = MoveOp> + '_ {
     let mut moves = Vec::new();
     loop {
-        if let Some((tag, count)) = tag_to_split(&files.tags_files) {
+        if let Some((tag, count)) = tag_to_split(files.tags_files()) {
             debug_assert_ne!(count, 0);
 
             let tag = tag.to_owned();
@@ -47,10 +31,23 @@ fn _organize(mut files: Files, prefix: Prefix) -> impl Iterator<Item = MoveOp> +
                     .collect(),
             ));
         } else {
-            debug_assert_eq!(files.tags_files.len(), 0);
+            debug_assert_eq!(files.tags_files().len(), 0);
             return moves.into_iter().chain(to_move_ops(files, prefix));
         }
     }
+}
+
+fn tag_to_split<'a>(tags_files: &'a TagsFiles) -> Option<(&'a TagRef, usize)> {
+    tags_files
+        .iter()
+        .map(|(tag, tag_files)| (tag, tag_files.len()))
+        .max_by(|(tag, count), (other_tag, other_count)| {
+            count
+                .cmp(other_count)
+                .then_with(|| tag.len().cmp(&other_tag.len()))
+                .then_with(|| tag.cmp(other_tag).reverse())
+        })
+        .map(|(tag, count)| (*tag, count))
 }
 
 fn to_move_ops(files: Files, prefix: Prefix) -> impl Iterator<Item = MoveOp> + '_ {
@@ -78,14 +75,14 @@ fn to_move_ops(files: Files, prefix: Prefix) -> impl Iterator<Item = MoveOp> + '
                 Ok(())
             }),
     ));
-    files
-        .untagged_files
+    let (untagged_files, inline_tags) = files.finalize();
+    untagged_files
         .into_iter()
         .map({
             let prefix = Rc::clone(&prefix);
             move |file| (format!("{}{TAG_END}{}", &prefix, file.name()).into(), file)
         })
-        .chain(files.inline_tags.into_iter().map(move |(file, mut tags)| {
+        .chain(inline_tags.into_iter().map(move |(file, mut tags)| {
             tags.sort_unstable_by(|tag, other| {
                 tag.len()
                     .cmp(&other.len())
@@ -114,90 +111,111 @@ fn to_move_ops(files: Files, prefix: Prefix) -> impl Iterator<Item = MoveOp> + '
         })
 }
 
-fn tag_to_split<'a>(tags_files: &'a TagsFiles) -> Option<(&'a TagRef, usize)> {
-    tags_files
-        .iter()
-        .map(|(tag, tag_files)| (tag, tag_files.len()))
-        .max_by(|(tag, count), (other_tag, other_count)| {
-            count
-                .cmp(other_count)
-                .then_with(|| tag.len().cmp(&other_tag.len()))
-                .then_with(|| tag.cmp(other_tag).reverse())
-        })
-        .map(|(tag, count)| (*tag, count))
-}
+mod files {
+    use std::collections::{HashMap, HashSet};
 
-#[derive(Debug)]
-struct Files<'a> {
-    tags_files: TagsFiles<'a>,
-    untagged_files: UntaggedFiles<'a>,
-    inline_tags: InlineTags<'a>,
-}
+    use by_address::ByThinAddress;
+    use smallvec::SmallVec;
 
-type TagsFiles<'a> = HashMap<&'a TagRef, HashSet<ByThinAddress<&'a TaggedFile>>>;
+    use crate::{TagRef, TaggedFile};
 
-type UntaggedFiles<'a> = Vec<ByThinAddress<&'a TaggedFile>>;
-
-type InlineTags<'a> = HashMap<ByThinAddress<&'a TaggedFile>, SmallVec<[&'a TagRef; 1]>>;
-
-impl<'a> Files<'a> {
-    /// Return (with_tag, without_tag).
-    pub fn partition(mut self, tag: &TagRef) -> (Self, Self) {
-        let files = self.tags_files.remove(tag).unwrap();
-        let mut tags_files: TagsFiles = HashMap::new();
-        let mut untagged_files = Vec::new();
-        let mut inline_tags = HashMap::new();
-        for file in files {
-            let mut added = false;
-            for tag in file.tags() {
-                if let Some(self_tag_files) = self.tags_files.get_mut(tag) {
-                    tags_files.entry(tag).or_default().insert(file);
-                    added = true;
-                    if self_tag_files.len() == 1 {
-                        self.tags_files.remove(tag);
-                    } else {
-                        self_tag_files.remove(&file);
-                    }
-                }
-            }
-            match self.inline_tags.remove(&file) {
-                Some(tags) => {
-                    inline_tags.insert(file, tags);
-                }
-                None => {
-                    if !added {
-                        untagged_files.push(file)
-                    }
-                }
-            }
-        }
-        extract_inline_tags_into(&mut tags_files, &mut inline_tags);
-        extract_inline_tags_into(&mut self.tags_files, &mut self.inline_tags);
-        (
-            Self {
-                tags_files,
-                untagged_files,
-                inline_tags,
-            },
-            self,
-        )
+    #[derive(Debug)]
+    pub struct Files<'a> {
+        tags_files: TagsFiles<'a>,
+        untagged_files: UntaggedFiles<'a>,
+        inline_tags: InlineTags<'a>,
     }
-}
 
-fn extract_inline_tags<'a>(tags_files: &mut TagsFiles<'a>) -> InlineTags<'a> {
-    let mut inline_tags: InlineTags = HashMap::new();
-    extract_inline_tags_into(tags_files, &mut inline_tags);
-    inline_tags
-}
+    pub type TagsFiles<'a> = HashMap<&'a TagRef, HashSet<ByThinAddress<&'a TaggedFile>>>;
 
-fn extract_inline_tags_into<'a>(tags_files: &mut TagsFiles<'a>, inline_tags: &mut InlineTags<'a>) {
-    tags_files.retain(|tag, tag_files| {
-        if tag_files.len() == 1 {
-            let file = *tag_files.iter().next().unwrap();
-            inline_tags.entry(file).or_default().push(tag);
-            false
-        } else {
-            true
+    type UntaggedFiles<'a> = Vec<ByThinAddress<&'a TaggedFile>>;
+
+    type InlineTags<'a> = HashMap<ByThinAddress<&'a TaggedFile>, SmallVec<[&'a TagRef; 1]>>;
+
+    impl<'a> Files<'a> {
+        pub fn new(files: &'a [TaggedFile]) -> Self {
+            let mut tags_files: TagsFiles = HashMap::new();
+            for file in files.iter().map(ByThinAddress) {
+                for tag in file.tags() {
+                    tags_files.entry(tag).or_default().insert(file);
+                }
+            }
+            Self {
+                inline_tags: extract_inline_tags(&mut tags_files),
+                tags_files,
+                untagged_files: Vec::new(), // Files with no tags will never move.
+            }
         }
-    });
+
+        /// Return (with_tag, without_tag).
+        pub fn partition(mut self, tag: &TagRef) -> (Self, Self) {
+            let files = self.tags_files.remove(tag).unwrap();
+            let mut tags_files: TagsFiles = HashMap::new();
+            let mut untagged_files = Vec::new();
+            let mut inline_tags = HashMap::new();
+            for file in files {
+                let mut added = false;
+                for tag in file.tags() {
+                    if let Some(self_tag_files) = self.tags_files.get_mut(tag) {
+                        tags_files.entry(tag).or_default().insert(file);
+                        added = true;
+                        if self_tag_files.len() == 1 {
+                            self.tags_files.remove(tag);
+                        } else {
+                            self_tag_files.remove(&file);
+                        }
+                    }
+                }
+                match self.inline_tags.remove(&file) {
+                    Some(tags) => {
+                        inline_tags.insert(file, tags);
+                    }
+                    None => {
+                        if !added {
+                            untagged_files.push(file)
+                        }
+                    }
+                }
+            }
+            extract_inline_tags_into(&mut tags_files, &mut inline_tags);
+            extract_inline_tags_into(&mut self.tags_files, &mut self.inline_tags);
+            (
+                Self {
+                    tags_files,
+                    untagged_files,
+                    inline_tags,
+                },
+                self,
+            )
+        }
+
+        pub fn tags_files(&self) -> &TagsFiles {
+            &self.tags_files
+        }
+
+        pub fn finalize(self) -> (UntaggedFiles<'a>, InlineTags<'a>) {
+            (self.untagged_files, self.inline_tags)
+        }
+    }
+
+    fn extract_inline_tags<'a>(tags_files: &mut TagsFiles<'a>) -> InlineTags<'a> {
+        let mut inline_tags: InlineTags = HashMap::new();
+        extract_inline_tags_into(tags_files, &mut inline_tags);
+        inline_tags
+    }
+
+    fn extract_inline_tags_into<'a>(
+        tags_files: &mut TagsFiles<'a>,
+        inline_tags: &mut InlineTags<'a>,
+    ) {
+        tags_files.retain(|tag, tag_files| {
+            if tag_files.len() == 1 {
+                let file = *tag_files.iter().next().unwrap();
+                inline_tags.entry(file).or_default().push(tag);
+                false
+            } else {
+                true
+            }
+        });
+    }
 }
