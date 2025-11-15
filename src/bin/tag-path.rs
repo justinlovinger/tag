d05b4a6f10c36d7d093b1a2ae3f5bfab650ac59f
Justin Lovinger <git@justinlovinger.com>
2025-11-15 23:08:20 +0000

    Rename `tag sort-tags-by-subfrequency` to `tag sort --method=subfrequency`

--- src/bin/tag-path.rs
+++ src/bin/tag-path.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 use itertools::Itertools;
 use tag::{combine, sort_tags_by_subfrequency, TaggedPath};
 
@@ -13,29 +13,43 @@ struct Args {
 enum Commands {
     /// Print tags in the given path
     Tags { path: TaggedPath },
-    /// Print the extension of the given path
+    /// Print extension of the given path
     Ext { path: TaggedPath },
-    /// Sort tags by descending frequency
+    /// Sort tags within each path
     ///
-    /// Frequency is calculated within each subset
+    /// Paths are returned in the order given.
+    ///
+    /// Paths may be returned without directory separators.
+    ///
+    /// Subfrequency is frequency calculated within each subset
     /// formed by selecting the next tag.
     /// For example,
-    /// `foo.x foo.x foo-bar.x foo-baz-bar.x baz.x baz.x`
-    /// is sorted into
-    /// `foo.x foo.x foo-bar.x foo-bar-baz.x baz.x baz.x`
+    /// in `foo-baz-bar.x foo.x foo.x foo-bar.x baz.x baz.x`,
+    /// `foo-baz-bar.x` is sorted as `foo-bar-baz.x`
     /// because `bar` is more common than `baz`
-    /// within the subset of paths containing `foo`.
-    ///
-    /// Paths are returned without directories.
-    SortTagsBySubfrequency {
+    /// within the subset of paths containing `foo`,
+    /// even though `baz` is more common globally.
+    Sort {
+        #[arg(long, short, value_enum, default_value_t=SortMethod::default())]
+        method: SortMethod,
+
         #[arg(value_name = "PATH")]
         paths: Vec<TaggedPath>,
     },
     /// Combine paths into optimal directories
+    ///
+    /// Paths are returned in the order given.
     Combine {
         #[arg(value_name = "PATH")]
         paths: Vec<TaggedPath>,
     },
+}
+
+#[derive(Copy, Clone, Default, ValueEnum)]
+enum SortMethod {
+    /// Sort tags by descending subfrequency
+    #[default]
+    Subfrequency,
 }
 
 fn main() {
@@ -44,8 +58,13 @@ fn main() {
     match args.command {
         Some(Commands::Tags { path }) => print!("{}", path.tags().format("\n")),
         Some(Commands::Ext { path }) => println!("{}", path.ext()),
-        Some(Commands::SortTagsBySubfrequency { paths }) => {
-            println!("{}", sort_tags_by_subfrequency(&paths).format("\n"))
+        Some(Commands::Sort { method, paths }) => {
+            println!(
+                "{}",
+                match method {
+                    SortMethod::Subfrequency => sort_tags_by_subfrequency(&paths).format("\n"),
+                }
+            )
         }
         Some(Commands::Combine { paths }) => {
             println!(
