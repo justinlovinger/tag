6806a15cb19efbe1a60ec5db79e1a0c42b9229a9
Justin Lovinger <git@justinlovinger.com>
2025-11-16 00:40:05 +0000

    Move commands from `tag-path` to `tag`

--- src/bin/tag.rs
+++ src/bin/tag.rs
@@ -5,8 +5,8 @@ use std::{
     path::PathBuf,
 };
 
-use clap::{Parser, Subcommand};
-use tag::{find, Tag};
+use clap::{Parser, Subcommand, ValueEnum};
+use tag::{combine, find, sort_tags_by_subfrequency, Tag, TaggedPath};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -26,16 +26,55 @@ struct Args {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Print tagged paths of files with given tags
+    /// Print tags in the given path
+    Tags { path: TaggedPath },
+    /// Print extension of the given path
+    Ext { path: TaggedPath },
+    /// Print tagged paths with given tags
     Find {
-        /// Find files including *all* these tags
+        /// Find paths including _all_ these tags
         #[arg(value_name = "TAG")]
         include: Vec<Tag>,
 
-        /// Ignore files including *any* of these tags
+        /// Ignore paths including _any_ of these tags
         #[arg(long, short, value_name = "TAG")]
         exclude: Vec<Tag>,
     },
+    /// Sort tags within each path
+    ///
+    /// Paths are returned in the order given.
+    ///
+    /// Paths may be returned without directory separators.
+    ///
+    /// Subfrequency is frequency calculated within each subset
+    /// formed by selecting the next tag.
+    /// For example,
+    /// in `foo-baz-bar.x foo.x foo.x foo-bar.x baz.x baz.x`,
+    /// `foo-baz-bar.x` is sorted as `foo-bar-baz.x`
+    /// because `bar` is more common than `baz`
+    /// within the subset of paths containing `foo`,
+    /// even though `baz` is more common globally.
+    Sort {
+        #[arg(long, short, value_enum, default_value_t=SortMethod::default())]
+        method: SortMethod,
+
+        #[arg(value_name = "PATH")]
+        paths: Vec<TaggedPath>,
+    },
+    /// Combine paths into optimal directories
+    ///
+    /// Paths are returned in the order given.
+    Combine {
+        #[arg(value_name = "PATH")]
+        paths: Vec<TaggedPath>,
+    },
+}
+
+#[derive(Copy, Clone, Default, ValueEnum)]
+enum SortMethod {
+    /// Sort tags by descending subfrequency
+    #[default]
+    Subfrequency,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -46,8 +85,19 @@ fn main() -> anyhow::Result<()> {
     }
 
     match args.command {
+        Some(Commands::Tags { path }) => print(path.tags())?,
+        Some(Commands::Ext { path }) => println!("{}", path.ext()),
         Some(Commands::Find { include, exclude }) => {
             print(find(current_dir()?, include, exclude)?)?
+        }
+        Some(Commands::Sort { method, paths }) => print(match method {
+            SortMethod::Subfrequency => sort_tags_by_subfrequency(&paths),
+        })?,
+        Some(Commands::Combine { paths }) => {
+            let mut out = stdout();
+            for path in combine(&paths) {
+                writeln!(out, "{}", path.display())?
+            }
         }
         None => {}
     }
@@ -59,13 +109,13 @@ fn print<T>(it: impl IntoIterator<Item = T>) -> io::Result<()>
 where
     T: fmt::Display,
 {
-    let mut out = writer();
+    let mut out = stdout();
     for item in it {
         writeln!(out, "{item}")?
     }
     Ok(())
 }
 
-fn writer() -> impl Write {
+fn stdout() -> impl Write {
     io::BufWriter::new(io::stdout().lock())
 }
